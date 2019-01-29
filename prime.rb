b010acd4d5749dd11271acde70fb0810f63c9c32
Patrick Kim <pkim050@gmail.com>
2019-01-29 12:34:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@
 def prime?(number)
   return false if number < 2
   counter = number - 1
-  for i in counter..2
-    return false if number % i == 0
+  while counter > 2
+    return false if counter % i
   end
   return true
 end
